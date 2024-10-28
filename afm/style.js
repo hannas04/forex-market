d51365b0a507a85f6b52ecf11d3561ee5640e70b
hannas04 <144782566+hannas04@users.noreply.github.com>
2024-10-28 17:18:22 -0400

    Add files via upload

---
+++ afm/style.js
@@ -0,0 +1,7 @@
+function showOverlay() {
+  document.getElementById("overlay").style.display = "flex";
+}
+
+function closeOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
